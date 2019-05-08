dfd786a2ff91763bbc16311e231a6444b474b6c1
Jan Lelis <mail@janlelis.de>
2019-05-08 09:28:24 +0200

    Release v1.8.0

--- lib/unicode/name/constants.rb
+++ lib/unicode/name/constants.rb
@@ -2,7 +2,7 @@
 
 module Unicode
   module Name
-    VERSION = "1.7.1"
+    VERSION = "1.8.0"
     UNICODE_VERSION = "12.1.0"
     DATA_DIRECTORY = File.expand_path(File.dirname(__FILE__) + "/../../../data/").freeze
     INDEX_FILENAME = (DATA_DIRECTORY + "/name.marshal.gz").freeze
