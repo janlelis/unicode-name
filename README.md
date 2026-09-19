53c067d2a0bf7474a481a0c4b3446c2600f7c3a1
Jan Lelis <hi@syntaxtransformation.com>
2026-09-19 17:25:31 +0200

    Update Unicode version number

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Return Unicode codepoint names, aliases, and labels.
 
-Unicode version: **17.0.0** (September 2025)
+Unicode version: **18.0.0** (September 2026)
 
 ## Usage
 
--- lib/unicode/name/constants.rb
+++ lib/unicode/name/constants.rb
@@ -3,7 +3,7 @@
 module Unicode
   module Name
     VERSION = "1.14.0"
-    UNICODE_VERSION = "17.0.0"
+    UNICODE_VERSION = "18.0.0"
     DATA_DIRECTORY = File.expand_path(File.dirname(__FILE__) + "/../../../data/").freeze
     INDEX_FILENAME = (DATA_DIRECTORY + "/name.marshal.gz").freeze
   end
